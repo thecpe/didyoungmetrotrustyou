c0d646d3960aba5ce54f27ab54614dc9889f65d2
Christian Espinosa <theoneandonlycpe@gmail.com>
2016-03-05 07:37:32 -0500

    a lot of updates

---
+++ button.js
@@ -0,0 +1,32 @@
+
+var contextClass = (window.AudioContext || window.webkitAudioContext || window.mozAudioContext || window.oAudioContext || window.msAudioContext);
+var dance = document.getElementById("everybodydance");
+if (contextClass) {
+  var context = new contextClass();
+} else {
+  onError;
+}
+var request = new XMLHttpRequest();
+request.open('GET', "https://dl.dropboxusercontent.com/content_link/l1m2vtAWx7U5eESsiRSEXhc1q6FdVYwhElozj91W2BmeRP5C3NGmfL3YuDiQWLpm/file?duc_id=dropbox_duc_id", true);
+request.responseType = 'arraybuffer';
+request.onload = function() {
+ context.decodeAudioData(request.response, function(theBuffer) {
+  buffer = theBuffer;
+  }, onError);
+}
+request.send();
+
+function onError() { console.log("Bad browser! No Web Audio API for you"); }
+
+function unpress() { dance.classList.remove("pressed"); }
+
+function playSound() {
+ dance.classList.add("pressed");
+  var source = context.createBufferSource();
+  source.buffer = buffer;
+ source.connect(context.destination);
+  source.start(0);
+  var delay = 2000;
+  setTimeout(unpress,delay);
+}
+dance.addEventListener('click', function(event) { playSound(); });
